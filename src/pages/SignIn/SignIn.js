5b486eab0ad4f32625597a9217cc0b18058e727e
Karthik Sivasubramaniam <karthik.sivasubramaniam@gmail.com>
2021-08-03 16:54:25 +0530

    wire signin up to Cognito

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./SignIn.scss";
 import AuthFragment from "../../components/AuthFragment/AuthFragment";
+import UserPool from '../../UserPool';
+import { AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js";
 
 const SignIn = () => {
   const onSignIn = (creds) => {
     console.log("signin:", creds);
+    
+    const user = new CognitoUser({
+      Username: creds.username,
+      Pool: UserPool
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: creds.username,
+      Password: creds.password
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        console.log("onSuccess:", data);
+      },
+      onFailure: (err) => {
+        console.log("onFailure:", err);
+      },
+      newPasswordRequired: (data) => {
+        console.log("newPasswordRequired", data);
+      }
+    })
   }
   return (
     <AuthFragment mode="signin" onSubmit={onSignIn} />
